964e902d24ea693c949bee9ee53e14c9369d56c4
Bryan Valencia <94031627+bry-val@users.noreply.github.com>
2024-04-21 13:03:13 -0500

    testing the rearchitecture

--- frontend/resources/card.js
+++ frontend/resources/card.js
@@ -2,7 +2,7 @@ export default {
   coverImage: 'https://t4.ftcdn.net/jpg/05/83/36/27/360_F_583362799_tZAdbG2ZKCd0KCjdIUF7ww7d8AlBPkOX.jpg',
   profileImage: 'https://i.imgur.com/CXfKULR.jpeg',
   fullName: 'Bryan Valencia',
-  profession: 'Cloud Developer',
+  profession: 'Cloud Engineer',
   cv: {
     url: 'https://drive.google.com/uc?export=download&id=1QWldDBGaa0hzBL71Dvt7ZiLWrFYmOzT5',
     icon: 'mdi-download'
