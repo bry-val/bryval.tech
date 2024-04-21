e2161e305150e977e5919576790c966bd9569131
Bryan Valencia <94031627+bry-val@users.noreply.github.com>
2024-04-21 13:08:41 -0500

    Update miscellaneous.js

--- frontend/resources/miscellaneous.js
+++ frontend/resources/miscellaneous.js
@@ -3,11 +3,11 @@ export const title = 'Bryan Valencia | Personal Page';
 export const meta = [
   {
     name: 'description',
-    content: 'Personal page of Bryan Valencia, a Cloud Developer based in Chicago, Illinois. Passionate about developing software and cloud solutions. Team player and always eager to learn new technologies.'
+    content: 'Personal page of Bryan Valencia, a Cloud Engineer based in Chicago, Illinois. Passionate about developing software and cloud solutions. Team player and always eager to learn new technologies.'
   },
   {
     name: 'keywords',
-    content: 'bryan,valencia,personal,site,cloud,developer,software,web,desktop,mobile,fast,reliable,resume'
+    content: 'bryan,valencia,personal,site,cloud,engineer, developer,software,web,desktop,mobile,fast,reliable,resume'
   }
 ];
 export const landingPage = 'about';
